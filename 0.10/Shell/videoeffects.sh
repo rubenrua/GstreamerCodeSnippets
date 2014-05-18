29f540356123a5bc8b2b00cd362df4d891da7ee9
Ruben Gonzalez <rubenrua@gmail.com>
2014-05-18 12:39:18 +0200

    Use correct gst-launch version in 0.10

--- 0.10/Shell/videoeffects.sh
+++ 0.10/Shell/videoeffects.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
 # edgetv: EdgeTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! edgetv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! edgetv ! ffmpegcolorspace ! autovideosink
 
 # agingtv: AgingTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! agingtv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! agingtv ! ffmpegcolorspace ! autovideosink
 
 # dicetv: DiceTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! dicetv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! dicetv ! ffmpegcolorspace ! autovideosink
 
 # warptv: WarpTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! warptv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! warptv ! ffmpegcolorspace ! autovideosink
 
 # shagadelictv: ShagadelicTV
-gst-launch v4l2src ! ffmpegcolorspace ! shagadelictv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! shagadelictv ! ffmpegcolorspace ! autovideosink
 
 # vertigotv: VertigoTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! vertigotv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! vertigotv ! ffmpegcolorspace ! autovideosink
 
 # revtv: RevTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! revtv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! revtv ! ffmpegcolorspace ! autovideosink
 
 # quarktv: QuarkTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! quarktv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! quarktv ! ffmpegcolorspace ! autovideosink
 
 # optv: OpTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! optv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! optv ! ffmpegcolorspace ! autovideosink
 
 # radioactv: RadioacTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! radioactv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! radioactv ! ffmpegcolorspace ! autovideosink
 
 # streaktv: StreakTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! streaktv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! streaktv ! ffmpegcolorspace ! autovideosink
 
 # rippletv: RippleTV effect
-gst-launch v4l2src ! ffmpegcolorspace ! rippletv ! ffmpegcolorspace ! autovideosink
+gst-launch-0.10 v4l2src ! ffmpegcolorspace ! rippletv ! ffmpegcolorspace ! autovideosink
 
