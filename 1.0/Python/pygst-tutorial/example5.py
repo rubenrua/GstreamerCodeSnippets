dcd5e54b417c4fb5e097dab63f5fed163a908006
Ruben Gonzalez <rubenrua@gmail.com>
2014-07-10 14:39:22 +0200

    Testing pygi-convert.sh

--- 1.0/Python/pygst-tutorial/example5.py
+++ 1.0/Python/pygst-tutorial/example5.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python
 
-import sys, os, thread, time
-import pygtk, gtk, gobject
-import pygst
-pygst.require("0.10")
-import gst
+import sys, os
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject, Gtk
 
-class GTK_Main:
+class GTK_Main(object):
       
     def __init__(self):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
         window.set_title("Vorbis-Player")
         window.set_default_size(500, -1)
-        window.connect("destroy", gtk.main_quit, "WM destroy")
-        vbox = gtk.VBox()
+        window.connect("destroy", Gtk.main_quit, "WM destroy")
+        vbox = Gtk.VBox()
         window.add(vbox)
-        self.entry = gtk.Entry()
-        vbox.pack_start(self.entry, False)
-        hbox = gtk.HBox()
+        self.entry = Gtk.Entry()
+        vbox.pack_start(self.entry, False, False, 0)
+        hbox = Gtk.HBox()
         vbox.add(hbox)
-        buttonbox = gtk.HButtonBox()
-        hbox.pack_start(buttonbox, False)
-        rewind_button = gtk.Button("Rewind")
+        buttonbox = Gtk.HButtonBox()
+        hbox.pack_start(buttonbox, False, False, 0)
+        rewind_button = Gtk.Button("Rewind")
         rewind_button.connect("clicked", self.rewind_callback)
         buttonbox.add(rewind_button)
-        self.button = gtk.Button("Start")
+        self.button = Gtk.Button("Start")
         self.button.connect("clicked", self.start_stop)
         buttonbox.add(self.button)
-        forward_button = gtk.Button("Forward")
+        forward_button = Gtk.Button("Forward")
         forward_button.connect("clicked", self.forward_callback)
         buttonbox.add(forward_button)
-        self.time_label = gtk.Label()
+        self.time_label = Gtk.Label()
         self.time_label.set_text("00:00 / 00:00")
         hbox.add(self.time_label)
         window.show_all()
         
-        self.player = gst.Pipeline("player")
-        source = gst.element_factory_make("filesrc", "file-source")
-        demuxer = gst.element_factory_make("oggdemux", "demuxer")
+        self.player = Gst.Pipeline("player")
+        source = Gst.ElementFactory.make("filesrc", "file-source")
+        demuxer = Gst.ElementFactory.make("oggdemux", "demuxer")
         demuxer.connect("pad-added", self.demuxer_callback)
-        self.audio_decoder = gst.element_factory_make("vorbisdec", "vorbis-decoder")
-        audioconv = gst.element_factory_make("audioconvert", "converter")
-        audiosink = gst.element_factory_make("autoaudiosink", "audio-output")
+        self.audio_decoder = Gst.ElementFactory.make("vorbisdec", "vorbis-decoder")
+        audioconv = Gst.ElementFactory.make("audioconvert", "converter")
+        audiosink = Gst.ElementFactory.make("autoaudiosink", "audio-output")
         
         self.player.add(source, demuxer, self.audio_decoder, audioconv, audiosink)
-        gst.element_link_many(source, demuxer)
-        gst.element_link_many(self.audio_decoder, audioconv, audiosink)
+        source.link(demuxer)
+        self.audio_decoder.link(audioconv)
+        audioconv.link(audiosink)
         
         bus = self.player.get_bus()
         bus.add_signal_watch()
@@ -57,56 +57,56 @@ class GTK_Main:
             if os.path.isfile(filepath):
                 self.button.set_label("Stop")
                 self.player.get_by_name("file-source").set_property("location", filepath)
-                self.player.set_state(gst.STATE_PLAYING)
+                self.player.set_state(Gst.State.PLAYING)
                 self.play_thread_id = thread.start_new_thread(self.play_thread, ())
             else:
                 self.play_thread_id = None
-                self.player.set_state(gst.STATE_NULL)
+                self.player.set_state(Gst.State.NULL)
                 self.button.set_label("Start")
                 self.time_label.set_text("00:00 / 00:00")
                 
     def play_thread(self):
         play_thread_id = self.play_thread_id
-        gtk.gdk.threads_enter()
+        Gdk.threads_enter()
         self.time_label.set_text("00:00 / 00:00")
-        gtk.gdk.threads_leave()
+        Gdk.threads_leave()
         
         while play_thread_id == self.play_thread_id:
             try:
                 time.sleep(0.2)
-                dur_int = self.player.query_duration(gst.FORMAT_TIME, None)[0]
+                dur_int = self.player.query_duration(Gst.Format.TIME, None)[0]
                 if dur_int == -1:
                     continue
                 dur_str = self.convert_ns(dur_int)
-                gtk.gdk.threads_enter()
+                Gdk.threads_enter()
                 self.time_label.set_text("00:00 / " + dur_str)
-                gtk.gdk.threads_leave()
+                Gdk.threads_leave()
                 break
             except:
                 pass
                 
         time.sleep(0.2)
         while play_thread_id == self.play_thread_id:
-            pos_int = self.player.query_position(gst.FORMAT_TIME, None)[0]
+            pos_int = self.player.query_position(Gst.Format.TIME, None)[0]
             pos_str = self.convert_ns(pos_int)
             if play_thread_id == self.play_thread_id:
-                gtk.gdk.threads_enter()
+                Gdk.threads_enter()
                 self.time_label.set_text(pos_str + " / " + dur_str)
-                gtk.gdk.threads_leave()
+                Gdk.threads_leave()
             time.sleep(1)
                 
     def on_message(self, bus, message):
         t = message.type
-        if t == gst.MESSAGE_EOS:
+        if t == Gst.MessageType.EOS:
             self.play_thread_id = None
-            self.player.set_state(gst.STATE_NULL)
+            self.player.set_state(Gst.State.NULL)
             self.button.set_label("Start")
             self.time_label.set_text("00:00 / 00:00")
-        elif t == gst.MESSAGE_ERROR:
+        elif t == Gst.MessageType.ERROR:
             err, debug = message.parse_error()
             print "Error: %s" % err, debug
             self.play_thread_id = None
-            self.player.set_state(gst.STATE_NULL)
+            self.player.set_state(Gst.State.NULL)
             self.button.set_label("Start")
             self.time_label.set_text("00:00 / 00:00")
             
@@ -115,14 +115,14 @@ class GTK_Main:
         pad.link(adec_pad)
         
     def rewind_callback(self, w):
-        pos_int = self.player.query_position(gst.FORMAT_TIME, None)[0]
+        pos_int = self.player.query_position(Gst.Format.TIME, None)[0]
         seek_ns = pos_int - (10 * 1000000000)
-        self.player.seek_simple(gst.FORMAT_TIME, gst.SEEK_FLAG_FLUSH, seek_ns)
+        self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, seek_ns)
         
     def forward_callback(self, w):
-        pos_int = self.player.query_position(gst.FORMAT_TIME, None)[0]
+        pos_int = self.player.query_position(Gst.Format.TIME, None)[0]
         seek_ns = pos_int + (10 * 1000000000)
-        self.player.seek_simple(gst.FORMAT_TIME, gst.SEEK_FLAG_FLUSH, seek_ns)
+        self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, seek_ns)
         
     def convert_ns(self, t):
         # This method was submitted by Sam Mason.
@@ -135,7 +135,9 @@ class GTK_Main:
         else:
             h,m = divmod(m, 60)
             return "%i:%02i:%02i" %(h,m,s)
-            
+
+GObject.threads_init()
+Gst.init(None)        
 GTK_Main()
-gtk.gdk.threads_init()
-gtk.main()
+Gtk.main()
+
