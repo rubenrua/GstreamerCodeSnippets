c53fca3ae7fd70ba21f81b9fd388c918f7f5aed8
mmacia <mmacia@visual-engin.com>
2016-03-22 15:55:20 +0100

    Added contributors file

---
+++ CONTRIBUTORS.md.py
@@ -0,0 +1,9 @@
+* **[Marc Macià](https://github.com/MarcMacia)**
+
+* **[Juan Luís Marí]**
+
+* **[Héctor Canto](https://github.com/hectorcanto)**
+
+
+* **[Rubén González](https://github.com/rubenrua)**
+  * Original Author
